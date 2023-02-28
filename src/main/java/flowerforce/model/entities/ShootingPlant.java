b041cf5f103d9dcf68d0a21b17388d52118e5b6d
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-02-28 11:22:14 +0100

    Added Javadoc to plants and bullet interfaces. Corrected checkstyle errors. Renamed PassivePlant to UtilityPlant

--- src/main/java/flowerforce/model/entities/ShootingPlant.java
+++ src/main/java/flowerforce/model/entities/ShootingPlant.java
@@ -2,6 +2,14 @@ package flowerforce.model.entities;
 
 import java.util.Optional;
 
-public interface ShootingPlant extends LivingEntity{
+/**
+ * Models a plant that shoots bullets.
+ */
+public interface ShootingPlant extends LivingEntity {
+
+    /**
+     * 
+     * @return a non-empty Optional if the next bullet has already been produced.
+     */
     Optional<Bullet> nextBullet();
 }
