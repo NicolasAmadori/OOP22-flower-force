5f244a9d98c4557a7aaad98f0bbf007cff357133
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-03-01 22:09:16 +0100

    Added a ShootingPlant implementation and a factory to generate ShootingPlants objects

---
+++ src/main/java/flowerforce/model/entities/ShootingPlantImpl.java
@@ -0,0 +1,51 @@
+package flowerforce.model.entities;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
+
+import flowerforce.common.Timer;
+import javafx.geometry.Point2D;
+
+/**
+ * Models a generical plant that shoots bullets.
+ */
+public class ShootingPlantImpl extends AbstractLivingEntity implements ShootingPlant {
+
+    private final Class<?> bulletClass;
+
+    /**
+     * 
+     * @param pos the initial position to place the entity in
+     * @param timer used to produce bullets/suns at the right time
+     * @param health the starting health of the entity
+     * @param bulletClass the class of the bullet type to generate
+     */
+    public ShootingPlantImpl(final Point2D pos, final Timer timer, final double health, final Class<?> bulletClass) {
+        super(pos, timer, health);
+        this.bulletClass = bulletClass;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Optional<Bullet> nextBullet() {
+        Bullet bullet;
+        try {
+            final Constructor<?> constr = this.bulletClass.getConstructor(Point2D.class);
+            bullet = (Bullet) constr.newInstance();
+        } catch (
+            InvocationTargetException
+            | SecurityException
+            | NoSuchMethodException
+            | InstantiationException 
+            | IllegalAccessException 
+            | IllegalArgumentException e
+        ) {
+            return Optional.empty();
+        }
+        return Optional.of(bullet).filter(e -> this.getTimer().isReady());
+    }
+
+}
