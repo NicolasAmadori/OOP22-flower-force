8508c2661fefe1f4a99bf8a13b13cc6da33b8bb0
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-03-01 19:35:15 +0100

    Modified Bullet interface, added bullets implementations

---
+++ src/main/java/flowerforce/model/entities/AbstractBullet.java
@@ -0,0 +1,74 @@
+package flowerforce.model.entities;
+
+import flowerforce.common.Timer;
+import javafx.geometry.Point2D;
+
+/**
+ * Models a generic bullet.
+ */
+public abstract class AbstractBullet extends AbstractActiveEntity implements Bullet {
+
+    private static final double STANDARD_DAMAGE = 40.0;
+    private static final int STANDARD_SPEED = 10;
+    private boolean hasHit;
+
+    /**
+     * 
+     * @param pos the initial position to place the bullet in
+     * @param timer used to move at the right time
+     * @param damage the damage that the bullet does to zombies
+     */
+    protected AbstractBullet(final Point2D pos, final Timer timer, final double damage) {
+        super(pos, timer, damage);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void move() {
+        this.setPosition(new Point2D(this.getPosition().getX() + 1, this.getPosition().getY()));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isOver() {
+        return this.hasHit;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double getDamage() {
+        this.hasHit = true;
+        return super.getDamage();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void hit(final Zombie zombie) {
+        zombie.receiveDamage(this.getDamage());
+    }
+
+    /**
+     * 
+     * @return the damage that a generic bullet does to a zombie.
+     */
+    protected static double getStandardDamage() {
+        return STANDARD_DAMAGE;
+    }
+
+    /**
+     * 
+     * @return the standard speed (in game loop cycles) of the bullet
+     */
+    protected static int getStandardSpeed() {
+        return STANDARD_SPEED;
+    }
+
+}
---
+++ src/main/java/flowerforce/model/entities/FireBullet.java
@@ -0,0 +1,30 @@
+package flowerforce.model.entities;
+
+import flowerforce.common.TimerImpl;
+import javafx.geometry.Point2D;
+
+/**
+ * Models a fire bullet that does additional damage but de-freezes a zombie when hit.
+ */
+public class FireBullet extends AbstractBullet {
+
+    private static final double ADDITIONAL_DAMAGE = 10.0;
+
+    /**
+     * 
+     * @param pos the position to place the bullet in
+     */
+    public FireBullet(final Point2D pos) {
+        super(pos, new TimerImpl(getStandardSpeed()), getStandardDamage() + ADDITIONAL_DAMAGE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void hit(final Zombie zombie) {
+        super.hit(zombie);
+        zombie.warmUp();
+    }
+
+}
