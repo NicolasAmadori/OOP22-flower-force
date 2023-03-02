37a0ae41b8865dc3f478173d0e911be804fd9b1c
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-03-02 15:25:02 +0100

    Renamed UtilityPlant in Sunflower and added its implementation

---
+++ src/main/java/flowerforce/model/entities/SunflowerImpl.java
@@ -0,0 +1,25 @@
+package flowerforce.model.entities;
+
+import flowerforce.common.TimerImpl;
+import javafx.geometry.Point2D;
+
+public class SunflowerImpl extends AbstractLivingEntity implements Sunflower {
+
+
+    private static final int STANDARD_SUNPRODUCING_TIME = 60;
+    private static final double STANDARD_SUNFLOWER_HEALTH = 100.0;
+
+    /**
+     * 
+     * @param pos the initial position to place the entity in
+     */
+    public SunflowerImpl(final Point2D pos) {
+        super(pos, new TimerImpl(STANDARD_SUNPRODUCING_TIME), STANDARD_SUNFLOWER_HEALTH);
+    }
+
+    @Override
+    public boolean isSunGenerated() {
+        return this.getTimer().isReady();
+    }
+    
+}
