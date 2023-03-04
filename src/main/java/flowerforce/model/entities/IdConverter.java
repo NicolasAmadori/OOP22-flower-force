8e52a2ab82008366fd65c0ee3834234f7c9d8189
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2023-03-04 18:31:07 +0100

    Implemented functional class IdConverter

--- src/main/java/flowerforce/model/entities/IdConverter.java
+++ src/main/java/flowerforce/model/entities/IdConverter.java
@@ -1,20 +1,114 @@
 package flowerforce.model.entities;
 
+import javafx.geometry.Point2D;
+
 /**
- * It allows converting from an ID to a type.
+ * This functional class can be used to obtain an {@link Entity} from its type identificator.
  */
-public interface IdConverter {
-    /**
-     * This method allows us to know which type of plant corresponds to a certain ID.
-     * @param idPlant ID of the plant for which we want to know the type
-     * @return The type of plant that corresponds to the passed ID
-     */
-    Entity plantType(int idPlant);
+public final class IdConverter {
+
+    private static final ZombieFactory ZOMBIE_FACTORY = new ZombieFactoryImpl();
+    private static final ShootingPlantFactory SHOOTING_PLANT_FACTORY = new ShootingPlantFactoryImpl();
+
+    private IdConverter() {
+    }
 
     /**
-     * This method allows us to know which type of zombie corresponds to a certain ID.
-     * @param idZombie ID of the zombie for which we want to know the type
-     * @return The type of zombie that corresponds to the passed ID
+     * Different plant types.
      */
-    Zombie zombieType(int idZombie);
+    public enum Plants {
+        /**
+         * Produces suns.
+         */
+        SUNFLOWER,
+        /**
+         * Shoots standard bullets.
+         */
+        COMMONSHOOTER,
+        /**
+         * Shoots special bullets that freeze zombies.
+         */
+        SNOWSHOOTER,
+        /**
+         * Shoots standard bullets with an high firing rate.
+         */
+        FASTSHOOTER,
+        /**
+         * Shoots special bullets that unfreeze zombies but gives them higher damage. 
+         */
+        FIRESHOOTER;
+    }
+
+    /**
+     * Different zombie types.
+     */
+    public enum Zombies {
+        /**
+         * Basic zombie, with default health and velocity.
+         */
+        BASIC,
+        /**
+         * Medium resistance zombie.
+         */
+        CONEHEAD,
+        /**
+         * High resistance zombie.
+         */
+        BUCKETHEAD,
+        /**
+         * High velocity zombie.
+         */
+        RUNNER,
+        /**
+         * High resistance and running zombie.
+         */
+        QUARTERBACK;
+    }
+
+    /**
+     * 
+     * @param plantType the type of plant
+     * @param pos the position of the plant
+     * @return a new placed plant of the specified type
+     */
+    public static Plant createPlant(final IdConverter.Plants plantType, final Point2D pos) {
+        switch (plantType) {
+            case SUNFLOWER:
+                return new SunflowerImpl(pos);
+            case COMMONSHOOTER:
+                return SHOOTING_PLANT_FACTORY.common(pos);
+            case SNOWSHOOTER:
+                return SHOOTING_PLANT_FACTORY.snow(pos);
+            case FASTSHOOTER:
+                return SHOOTING_PLANT_FACTORY.fast(pos);
+            case FIRESHOOTER:
+                return SHOOTING_PLANT_FACTORY.fire(pos);
+            default:
+                throw new IllegalStateException("ERROR: plant type has not been identified");
+        }
+    }
+
+    /**
+     * 
+     * @param zombieType the type of zombie
+     * @param pos the position of the zombie
+     * @return a new placed zombie of the specified type
+     */
+    public static Zombie createZombie(final IdConverter.Zombies zombieType, final Point2D pos) {
+        switch (zombieType) {
+            case BASIC:
+                return ZOMBIE_FACTORY.basic(pos);
+            case CONEHEAD:
+                return ZOMBIE_FACTORY.conehead(pos);
+            case BUCKETHEAD:
+                return ZOMBIE_FACTORY.buckethead(pos);
+            case RUNNER:
+                return ZOMBIE_FACTORY.running(pos);
+            case QUARTERBACK:
+                return ZOMBIE_FACTORY.quarterback(pos);
+            default:
+                throw new IllegalStateException("ERROR: zombie type has not been identified");
+        }
+    }
+
 }
