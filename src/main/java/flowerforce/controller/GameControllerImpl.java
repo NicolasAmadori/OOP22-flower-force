3dae54ef00ecbdf1075c9eceeb8d4a9f8d593ff8
Nicolas Amadori <nicolasamadori2002@gmail.com>
2023-03-04 20:31:13 +0100

    Aggiunto evend handler per la le imgView della gridPane

--- src/main/java/flowerforce/controller/GameControllerImpl.java
+++ src/main/java/flowerforce/controller/GameControllerImpl.java
@@ -1,25 +1,48 @@
 package flowerforce.controller;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Map;
+import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
 
 /**
  * {@inheritDoc}.
  */
-public class GameControllerImpl implements GameController {
+public class GameControllerImpl implements GameController, Initializable {
 
     @FXML
-    public void provaClick(ActionEvent e) {
-        System.out.println("Click bottone avvenuto");
+    private AnchorPane gamePane;
+
+    @FXML
+    private Button btnProva;
+
+    @FXML
+    private ImageView imgBackground;
+
+    @FXML
+    private GridPane griglia;
+
+    @FXML
+    void entered(MouseEvent event) {
+        System.out.println("ARGH");
     }
 
     @FXML
-    public void regionClick(MouseEvent e) throws IOException {
-        System.out.println("Click region avvenuto");
+    void regionClicked(ActionEvent event) {
+        System.out.println("ARGH");
     }
 
     /**
@@ -58,4 +81,33 @@ public class GameControllerImpl implements GameController {
         throw new UnsupportedOperationException("Unimplemented method 'startInfiniteGame'");
     }
 
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  {@code null} if the location is not known.
+     * @param resources The resources used to localize the root object, or {@code null} if
+     *                  the root object was not localized.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //setWindowSize();
+
+        griglia.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(javafx.scene.input.MouseEvent event) {
+                Node source = (Node) event.getTarget();
+                System.out.println(griglia.getColumnIndex(source) + " " + griglia.getRowIndex(source));
+            }
+        });
+    }
+
+    private void setWindowSize() {
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        gamePane.setPrefWidth(screenSize.getWidth() - 200);
+        gamePane.setPrefHeight(screenSize.getHeight() - 200);
+        imgBackground.setFitWidth(gamePane.getPrefWidth());
+        imgBackground.setFitHeight(gamePane.getPrefHeight());
+    }
 }
--- src/main/java/flowerforce/view/GameViewImpl.java
+++ src/main/java/flowerforce/view/GameViewImpl.java
@@ -1,9 +1,11 @@
 package flowerforce.view;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class GameViewImpl extends Application implements GameView  {
@@ -16,6 +18,12 @@ public class GameViewImpl extends Application implements GameView  {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(ClassLoader.getSystemResource("flowerforce/game/Garden.fxml"));
         Scene scene = new Scene(root);
+        scene.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                //System.out.println("mouse click detected! " + mouseEvent.getTarget());
+            }
+        });
 
         primaryStage.setTitle("Titolo prova");
         primaryStage.setScene(scene);
