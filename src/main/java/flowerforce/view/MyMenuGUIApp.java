2fb53b95b0a0da2227d4fe1b12d1f35bf26fa764
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-03-06 11:29:55 +0100

    Moved styling to fxml files

--- src/main/java/flowerforce/view/MyMenuGUIApp.java
+++ src/main/java/flowerforce/view/MyMenuGUIApp.java
@@ -11,7 +11,7 @@ public class MyMenuGUIApp extends Application{
     public void start(final Stage primaryStage) throws Exception {
         final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("flowerforce/game/SimpleMenu.fxml"));
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("/css/styling.css").toExternalForm());
+        // scene.getStylesheets().add(getClass().getResource("/css/styling.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setTitle("Hello");
         primaryStage.show();
--- src/main/java/flowerforce/view/SceneManager.java
+++ src/main/java/flowerforce/view/SceneManager.java
@@ -3,10 +3,12 @@ package flowerforce.view;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class SceneManager {
@@ -15,10 +17,14 @@ public class SceneManager {
     private Scene scene;
     private Parent root;
 
-    public void startLvl1(ActionEvent e) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("flowerforce/game/TempScene.fxml"));
-        Stage stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+    @FXML
+    void start(MouseEvent event) throws IOException {
+        System.out.println("Bottone cliccato");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("flowerforce/game/TempScene.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
         stage.show();
     }
 }
