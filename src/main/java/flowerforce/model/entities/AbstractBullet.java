13845bc8fbb6cf1f8d2f6e407c9d3d898c57e6ef
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-03-09 20:31:06 +0100

    Corrected bullets' position update in AbstractBullet

--- src/main/java/flowerforce/model/entities/AbstractBullet.java
+++ src/main/java/flowerforce/model/entities/AbstractBullet.java
@@ -27,7 +27,7 @@ public abstract class AbstractBullet extends AbstractEntity implements Bullet {
      */
     @Override
     public void move() {
-        this.setPosition(new Point2D(this.getPosition().getX(), this.getPosition().getY() + DELTA));
+        this.setPosition(new Point2D(this.getPosition().getX() + DELTA, this.getPosition().getY()));
     }
 
     /**
