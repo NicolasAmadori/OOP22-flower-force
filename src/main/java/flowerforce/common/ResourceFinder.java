87fc8bfa927fbdcd6dd35f4fd2a5e2694a97c969
Nicolas Amadori <nicolasamadori2002@gmail.com>
2023-03-20 22:57:43 +0100

    resolved checkstyle and pmd errors

--- src/main/java/flowerforce/common/ResourceFinder.java
+++ src/main/java/flowerforce/common/ResourceFinder.java
@@ -1,27 +1,50 @@
 package flowerforce.common;
 
-import javax.swing.text.html.Option;
 import java.io.File;
 import java.util.Optional;
 
-public class ResourceFinder {
+/**
+ * Utility class to get resources complete path.
+ */
+public final class ResourceFinder {
 
-    private static String PROJECT_FOLDER_ABSOLUTE_PATH = System.getProperty("user.dir");
-    private static String RESOURCES_FOLDER_PATH = "src" + File.separator + "main" + File.separator + "resources"
-            + File.separator + "flowerforce" + File.separator + "game";
-    private static String RESOURCES_FOLDER_ABSOLUTE_PATH = PROJECT_FOLDER_ABSOLUTE_PATH + File.separator + RESOURCES_FOLDER_PATH;
+    private static final String PROJECT_FOLDER_ABSOLUTE_PATH = System.getProperty("user.dir");
+    private static final String RESOURCES_FOLDER_PATH = "src" + File.separator + "main" + File.separator
+            + "resources" + File.separator + "flowerforce" + File.separator + "game";
+    private static final String RESOURCES_FOLDER_ABSOLUTE_PATH = PROJECT_FOLDER_ABSOLUTE_PATH
+            + File.separator + RESOURCES_FOLDER_PATH;
 
-    private static String IMAGES_FOLDER_NAME = "images";
-    private static String FXML_FOLDER_NAME = "fxml";
-    private static String SAVING_FOLDER_NAME = "savings";
+    private static final String IMAGES_FOLDER_NAME = "images";
+    private static final String FXML_FOLDER_NAME = "fxml";
+    private static final String SAVING_FOLDER_NAME = "savings";
+
+    private ResourceFinder() {
+
+    }
+
+    /**
+     * Get the complete path of an image.
+     * @param filename The complete name of the image requested
+     * @return An optional of the image path, empty if the requested image do not exist in the images folder
+     */
     public static Optional<String> getImagePath(final String filename) {
         return returnOptional(RESOURCES_FOLDER_ABSOLUTE_PATH + File.separator + IMAGES_FOLDER_NAME + File.separator + filename);
     }
 
+    /**
+     * Get the complete path of a fxml file.
+     * @param filename The complete name of the fxml file requested
+     * @return An optional of the fxml file path, empty if the requested fxml file do not exist in the fxml folder
+     */
     public static Optional<String> getFXMLPath(final String filename) {
         return returnOptional(RESOURCES_FOLDER_ABSOLUTE_PATH + File.separator + FXML_FOLDER_NAME + File.separator + filename);
     }
 
+    /**
+     * Get the complete path of a saving file.
+     * @param filename The complete name of the saving file requested
+     * @return An optional of the saving file path, empty if the requested saving file do not exist in the savings folder
+     */
     public static Optional<String> getSavingFilePath(final String filename) {
         return returnOptional(RESOURCES_FOLDER_ABSOLUTE_PATH + File.separator + SAVING_FOLDER_NAME + File.separator + filename);
     }
