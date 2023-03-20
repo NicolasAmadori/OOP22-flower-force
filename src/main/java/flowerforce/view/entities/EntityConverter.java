64080fd20a46c6f7e2f2379cac7a04c90b63ab4f
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2023-03-20 13:51:48 +0100

    resolved checkstyle for EntityConverter

--- src/main/java/flowerforce/view/entities/EntityConverter.java
+++ src/main/java/flowerforce/view/entities/EntityConverter.java
@@ -7,28 +7,50 @@ import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
 import java.io.File;
 
+/**
+ * This utility class convert an entity given from the model into an entity that can be drawn in the view.
+ */
 public final class EntityConverter {
-    private final static String IMAGES_FOLDER_PATH = System.getProperty("user.dir") + File.separator
+    private static final String IMAGES_FOLDER_PATH = System.getProperty("user.dir") + File.separator
             + "src" + File.separator + "main" + File.separator + "resources"
             + File.separator + "flowerforce" + File.separator + "game" + File.separator + "images";
 
-    private final static String IMAGES_EXTENSION = ".gif";
+    private static final String IMAGES_EXTENSION = ".gif";
 
-    public static EntityView getEntityView(Plant p) {
+    private EntityConverter() {
+
+    }
+
+    /**
+     * Convert a plant into an entityView.
+     * @param p The plant instance to convert
+     * @return The entityView representing the plant
+     */
+    public static EntityView getEntityView(final Plant p) {
         final String completeimagePath = IMAGES_FOLDER_PATH + File.separator
                 + p.getPlantType().name().toLowerCase() + IMAGES_EXTENSION;
         final Point2D newPosition = convertPlantPosition(p.getPosition(), completeimagePath);
         return new EntityViewImpl(newPosition, completeimagePath);
     }
 
-    public static EntityView getEntityView(Zombie z) {
+    /**
+     * Convert a zombie into an entityView.
+     * @param z The zombie instance to convert
+     * @return The entityView representing the zombie
+     */
+    public static EntityView getEntityView(final Zombie z) {
         final String completeImagePath = IMAGES_FOLDER_PATH + File.separator
                 + z.getZombieType().name().toLowerCase() + IMAGES_EXTENSION;
         final Point2D newPosition = convertZombiePosition(z.getPosition(), completeImagePath);
         return new EntityViewImpl(newPosition, completeImagePath);
     }
 
-    public static EntityView getEntityView(Bullet b) {
+    /**
+     * Convert a bullet into an entityView.
+     * @param b The bullet instance to convert
+     * @return The entityView representing the bullet
+     */
+    public static EntityView getEntityView(final Bullet b) {
         final String bulletName = getName(b.getClass().getName().toLowerCase());
         final String completeImagePath = IMAGES_FOLDER_PATH + File.separator
                 + bulletName + IMAGES_EXTENSION;
@@ -36,7 +58,7 @@ public final class EntityConverter {
         return new EntityViewImpl(newPosition, completeImagePath);
     }
 
-    private static String getName(String completePackage) {
+    private static String getName(final String completePackage) {
         String[] splitted = completePackage.split("\\.");
         return splitted[splitted.length - 1];
     }
@@ -53,19 +75,17 @@ public final class EntityConverter {
     }
 
     private static double getImageWidth(final String path) {
-        try{
+        try {
             return new Image(path).getWidth();
-        }
-        catch(Exception ex) {
+        } catch (Exception ex) {
             return -1;
         }
     }
 
     private static double getImageHeight(final String path) {
-        try{
+        try {
             return new Image(path).getHeight();
-        }
-        catch(Exception ex) {
+        } catch (Exception ex) {
             return -1;
         }
     }
