d1354a107f1778a7bb1de3263e3513db00fb3474
Nicolas Amadori <nicolasamadori2002@gmail.com>
2023-03-25 11:58:37 +0100

    Added static class to get rendering informations

---
+++ src/main/java/flowerforce/common/RenderingInformation.java
@@ -0,0 +1,14 @@
+package flowerforce.common;
+
+
+public class RenderingInformation {
+
+    private static final int framesPerSecond = 30;
+
+    private RenderingInformation() {
+
+    }
+    public static int getFramesPerSecond() {
+        return framesPerSecond;
+    }
+}
