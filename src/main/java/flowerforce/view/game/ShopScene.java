eef4bca7eacb85267b813ccd78d20b79146f4aa3
Juri Guglielmi <juri.guglielmi@studio.unibo.it>
2023-03-28 23:12:36 +0200

    start creatio of the shop view and controller

---
+++ src/main/java/flowerforce/view/game/ShopScene.java
@@ -0,0 +1,16 @@
+package flowerforce.view.game;
+
+import java.io.IOException;
+
+public class ShopScene extends AbstractFlowerForceScene {
+    private static final String FXML_PATH = "flowerforce/game/fxml/Shop.fxml";
+    private static final String IMAGE_NAME = "shop.png";
+
+    /**
+      * @param application which sets the scene
+      * @throws IOException if some files couldn't be found
+     */
+    protected ShopScene(final FlowerForceApplication application) throws IOException {
+        super(FXML_PATH, IMAGE_NAME, new GameSceneController(application));
+    }
+}
---
+++ src/main/java/flowerforce/view/game/ShopSceneController.java
@@ -0,0 +1,13 @@
+package flowerforce.view.game;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+import javax.swing.text.html.ImageView;
+
+public class ShopSceneController {
+
+    @FXML
+    private ImageView imgItemToSell;
+
+}
