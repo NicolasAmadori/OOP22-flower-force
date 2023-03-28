d238ad61d7d8dc5c56204cdd106e8c36fcf5a951
Nicolas Amadori <nicolasamadori2002@gmail.com>
2023-03-29 00:45:58 +0200

    implemented Shop and corrected checkstyle

--- src/main/java/flowerforce/model/game/ShopImpl.java
+++ src/main/java/flowerforce/model/game/ShopImpl.java
@@ -1,35 +1,49 @@
 package flowerforce.model.game;
 import flowerforce.model.entities.IdConverter;
+import javafx.util.Pair;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+/**
+ * This is an implementation of {@link Shop}
+ */
 public class ShopImpl implements Shop{
     private Player player;
-    private Map<BuyablePlant, Boolean> buyablePlants;
+    private Map<IdConverter.Plants, Pair<Integer, Boolean>> buyablePlants;
 
+    /**
+     * This is a constructor for a new shop instance.
+     * @param player The player to add bought plants to
+     */
     public ShopImpl(Player player) {
         this.player = player;
-        buyablePlants = Map.of(new BuyablePlant(IdConverter.Plants.FASTSHOOTER, 200), false);
-    }
-    @Override
-    public Map<BuyablePlant, Boolean> getPlants() {
-        return this.buyablePlants.entrySet().stream()
-                .map(e -> new Map.Entry<>() {});
+        buyablePlants = Map.of(IdConverter.Plants.CHERRYBOMB, new Pair<>(300, false));
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Map<Integer, Pair<Integer, Boolean>> getPlants() {
+        return this.buyablePlants.entrySet().stream()
+                .collect(Collectors.toMap(
+                        e -> e.getKey().ordinal(),
+                        e -> e.getValue()
+                ));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean buyPlant(int id) {
-        BuyablePlant boughtPlant = null;
-        for (var entry : this.buyablePlants.entrySet()) {
-            if(entry.getKey().getId() == id && entry.getKey().getCost() <= this.player.getCoins()) {
-                boughtPlant = entry.getKey();
+        if(this.buyablePlants.containsKey(IdConverter.Plants.values()[id])) {
+            if(this.player.subtractCoins(IdConverter.Plants.values()[id].getCost())) {
+                this.player.addPlant(IdConverter.Plants.values()[id]);//TODO: merge subtractCoins and addPlant
+                this.buyablePlants.compute(IdConverter.Plants.values()[id], (k, v) -> new Pair<>(v.getKey(), true));
+                return true;
             }
         }
-        if(boughtPlant.equals(null)) {
-            return false;
-        }
-        this.player.subtractCoins(boughtPlant.getCost());
-        this.player.addPlant(boughtPlant.getId());
-        this.buyablePlants.put(boughtPlant, true);
-        return true;
+        return false;
     }
 }
