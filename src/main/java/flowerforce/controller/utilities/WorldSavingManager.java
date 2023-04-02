d4c2234906866b8b9553d69acec919b47401c070
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2023-04-02 18:14:24 +0200

    removed level serialization

--- src/main/java/flowerforce/controller/utilities/WorldSavingManager.java
+++ src/main/java/flowerforce/controller/utilities/WorldSavingManager.java
@@ -18,8 +18,6 @@ import java.util.Optional;
  */
 public final class WorldSavingManager {
     private static final String PLAYER_FILE_NAME = "player";
-    private static final String INFINITELEVEL_FILE_NAME = "infiniteLevel";
-    private static final String LEVEL_FILE_PREFIX = "level";
 
     private WorldSavingManager() {
 
@@ -33,11 +31,7 @@ public final class WorldSavingManager {
     public static World load() throws InstantiationException {
         final Optional<Player> p = loadPlayer();
 
-        final Level infiniteLevel = loadInfiniteLevel();
-
-        final List<Level> levels = loadLevels();
-
-        return new WorldImpl(p, levels, infiniteLevel);
+        return new WorldImpl(p);
     }
 
     /**
@@ -52,39 +46,5 @@ public final class WorldSavingManager {
     private static Optional<Player> loadPlayer() {
         final SaveManager<Player> playerSaveManager = new SaveManager(PlayerImpl.class, PLAYER_FILE_NAME);
         return playerSaveManager.load();
-    }
-
-    private static Level loadInfiniteLevel() throws InstantiationException {
-        final SaveManager<Level> infiniteLevelSaveManager = new SaveManager(LevelImpl.class, INFINITELEVEL_FILE_NAME);
-        final Optional<Level> infiniteLevel = infiniteLevelSaveManager.load();
-        if (infiniteLevel.isEmpty()) {
-            throw new InstantiationException("Infinite level file has not been found.");
-        }
-        return infiniteLevel.get();
-    }
-
-    private static List<Level> loadLevels() throws InstantiationException {
-        final File savingFolder = new File(ResourceFinder.getSavingFolderPath());
-        final List<String> levelNames = Arrays.stream(savingFolder.listFiles())
-                .map(f -> f.getName())//get just the names
-                .map(n -> n.split("\\..")[0])//Remove extension from the names
-                .filter(f -> f.startsWith(LEVEL_FILE_PREFIX))//Get just levels names
-                .toList();
-
-        final List<Level> levels = new ArrayList<>();
-
-        SaveManager<Level> levelSaveManager;
-        for (final String levelName : levelNames) {
-            levelSaveManager = new SaveManager(LevelImpl.class, levelName);
-
-            final Optional<Level> loadedLevel = levelSaveManager.load();
-            loadedLevel.ifPresent(l -> levels.add(l));
-        }
-
-        if (levels.isEmpty()) {
-            throw new InstantiationException("Not enough levels are saved in the saving path.");
-        }
-
-        return levels;
     }
 }
