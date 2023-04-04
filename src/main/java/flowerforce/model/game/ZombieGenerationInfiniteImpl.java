2cf4f1997b8137b3ec303e7769ecda0f603e05d1
Juri Guglielmi <juri.guglielmi@studio.unibo.it>
2023-04-04 16:33:57 +0200

    Implements number of zombie before horde

--- src/main/java/flowerforce/model/game/ZombieGenerationInfiniteImpl.java
+++ src/main/java/flowerforce/model/game/ZombieGenerationInfiniteImpl.java
@@ -9,6 +9,7 @@ import java.util.*;
  */
 public class ZombieGenerationInfiniteImpl extends AbstractZombieGeneration {
 
+    private static final int ZOMBIE_BEFORE_HORDE = 7;
     private static final int MAX_ZOMBIE_TO_SPAWN_HORDE = 30;
     private static final int INC_ZOMBIE_HORDE = 5;
     private static final int START_NUMBER_ZOMBIE_IN_HORDE = 5;
@@ -17,7 +18,7 @@ public class ZombieGenerationInfiniteImpl extends AbstractZombieGeneration {
      * @param levelId of the game started
      */
     public ZombieGenerationInfiniteImpl(final int levelId) {
-        super(levelId, START_NUMBER_ZOMBIE_IN_HORDE);
+        super(levelId, ZOMBIE_BEFORE_HORDE, START_NUMBER_ZOMBIE_IN_HORDE);
     }
 
     /**
