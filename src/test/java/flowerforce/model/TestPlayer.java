f0a662be376d13c7d7469cc5b48f5b0b853afa46
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2023-04-05 15:34:57 +0200

    resolved checkstyle or suppressed

--- src/test/java/flowerforce/model/TestPlayer.java
+++ src/test/java/flowerforce/model/TestPlayer.java
@@ -11,11 +11,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 final class TestPlayer {
 
     private static final int STARTING_COINS = 0;
-    private static final int COINS1 = 50;
-    private static final int COINS2 = 100;
-    private static final int SCORE1 = 4_000;
-    private static final int SCORE2 = 1_500;
-    private static final int SCORE3 = 10_000;
+    private static final int FIRST_SCORE_TO_ADD = 4_000;
+    private static final int SECOND_SCORE_TO_ADD = 1_500;
+    private static final int THIRD_SCORE_TO_ADD = 10_000;
 
     private Player player;
 
@@ -32,16 +30,32 @@ final class TestPlayer {
      */
     @Test
     void testCoins() {
-        assertEquals(STARTING_COINS, this.player.getCoins());
+        int playerCoins = STARTING_COINS;
+        //at first the player coins must be 0
+        assertEquals(playerCoins, this.player.getCoins());
 
-        this.player.addCoins(COINS2);
-        assertEquals(COINS2, this.player.getCoins());
+        //you should add 100 coins with no problem
+        //CHECKSTYLE: MagicNumber OFF
+        //100 is a random number
+        this.player.addCoins(100);
+        playerCoins += 100;
+        //CHECKSTYLE: MagicNumber OFF
+        assertEquals(playerCoins, this.player.getCoins());
 
-        assertTrue(this.player.subtractCoins(COINS1));
-        assertEquals(COINS1, this.player.getCoins());
+        //you should subtract 50 from 100 with no problem
+        //CHECKSTYLE: MagicNumber OFF
+        //50 is a random number
+        assertTrue(this.player.subtractCoins(50));
+        playerCoins -= 50;
+        //CHECKSTYLE: MagicNumber ON
+        assertEquals(playerCoins, this.player.getCoins());
 
-        assertFalse(this.player.subtractCoins(COINS2));
-        assertEquals(COINS1, this.player.getCoins());
+        //you shouldn't be able to subtract 70 from 50 remaining coins
+        //CHECKSTYLE: MagicNumber OFF
+        //70 is a random number
+        assertFalse(this.player.subtractCoins(70));
+        //CHECKSTYLE: MagicNumber ON
+        assertEquals(playerCoins, this.player.getCoins());
     }
 
     /**
@@ -49,15 +63,23 @@ final class TestPlayer {
      */
     @Test
     void testScore() {
-        assertEquals(STARTING_COINS, this.player.getScoreRecord());
+        int playerScoreRecord = 0;
+        //The initial score record must be 0
+        assertEquals(playerScoreRecord, this.player.getScoreRecord());
 
-        this.player.addNewScore(SCORE1);
-        assertEquals(SCORE1, this.player.getScoreRecord());
+        //Adding a score of 4000, 4000 must be the new score record
+        this.player.addNewScore(FIRST_SCORE_TO_ADD);
+        playerScoreRecord = Math.max(playerScoreRecord, FIRST_SCORE_TO_ADD);
+        assertEquals(playerScoreRecord, this.player.getScoreRecord());
 
-        this.player.addNewScore(SCORE2);
-        assertEquals(SCORE1, this.player.getScoreRecord());
+        //adding a score of 1500, 4000 must still be the score record
+        this.player.addNewScore(SECOND_SCORE_TO_ADD);
+        playerScoreRecord = Math.max(playerScoreRecord, SECOND_SCORE_TO_ADD);
+        assertEquals(playerScoreRecord, this.player.getScoreRecord());
 
-        this.player.addNewScore(SCORE3);
-        assertEquals(SCORE3, this.player.getScoreRecord());
+        //adding a score of 10000, 10000 must be the new score record
+        this.player.addNewScore(THIRD_SCORE_TO_ADD);
+        playerScoreRecord = Math.max(playerScoreRecord, THIRD_SCORE_TO_ADD);
+        assertEquals(playerScoreRecord, this.player.getScoreRecord());
     }
 }
