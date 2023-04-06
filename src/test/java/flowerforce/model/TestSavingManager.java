05cc73bf51f0796034304ed97ed09b87738cc756
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2023-04-06 23:53:29 +0200

    temporary solution to remove spotbug errors

--- src/test/java/flowerforce/model/TestSavingManager.java
+++ src/test/java/flowerforce/model/TestSavingManager.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 final class TestSavingManager {
 
     private static final String FILE_NAME = "player";
-    private Player player;
+    private Player player = new PlayerImpl();
 
     /**
      * Sets up the testing.
@@ -28,6 +28,12 @@ final class TestSavingManager {
     @Test
     void testSaving() {
         final SaveManager<PlayerImpl> playerSaveManager = new SaveManager<>(PlayerImpl.class, FILE_NAME);
+
+        //random operation to modify player values
+        this.player.addCoins(160);
+        this.player.addNewScore(56560);
+        this.player.unlockedNextLevel();
+        this.player.unlockedNextLevel();
         playerSaveManager.save((PlayerImpl) this.player); //cast to save the player
 
         final Optional<PlayerImpl> newPlayer = playerSaveManager.load();
