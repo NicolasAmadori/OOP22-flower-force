24241df78685273bf170087ebf29e71db877eaf2
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-04-06 20:33:20 +0200

    Modified some plants' values

--- src/main/java/flowerforce/model/entities/CherryBomb.java
+++ src/main/java/flowerforce/model/entities/CherryBomb.java
@@ -12,10 +12,10 @@ import javafx.geometry.Point2D;
  */
 public class CherryBomb extends AbstractPlant implements ExplodingPlant {
 
-    private static final int CHERRY_HEALTH = 1;
+    private static final int CHERRY_HEALTH = 3000;
     private static final int CHERRY_COST = 150;
     private static final int DAMAGE = 1800;
-    private static final int CHERRY_RADIUS = 1;
+    private static final int CHERRY_RADIUS = 2;
     private static final double SECONDS_BEFORE_EXPLOSION = 1.2;
     private static final String CHERRY_NAME = "cherrybomb";
 
