94afa74ea6f2914bdae33d6f872f8a7290cf1714
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2023-04-08 14:09:37 +0200

    refactored ResourceFinder with getResource and implemented TestResourceFinder

--- src/main/java/flowerforce/view/game/GameSceneController.java
+++ src/main/java/flowerforce/view/game/GameSceneController.java
@@ -385,9 +385,9 @@ public final class GameSceneController implements GameEngine {
         this.removeBloomEffect();
         this.cards.keySet().forEach(card -> card.setDisable(true));
         if (isWon) {
-            imageResult.setImage(new Image(ResourceFinder.getCommonImagePath("victory.png")));
+            imageResult.setImage(new Image(ResourceFinder.getCommonImagePath("victory.png").toExternalForm()));
         } else {
-            imageResult.setImage(new Image(ResourceFinder.getCommonImagePath("loss.png")));
+            imageResult.setImage(new Image(ResourceFinder.getCommonImagePath("loss.png").toExternalForm()));
         }
     }
 }
