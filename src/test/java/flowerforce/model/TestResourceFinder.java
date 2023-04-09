6c6435a9d2fbdb48375bb65cc5e8d2ee5251f60b
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2023-04-09 23:33:47 +0200

    resolved checkstyle

--- src/test/java/flowerforce/model/TestResourceFinder.java
+++ src/test/java/flowerforce/model/TestResourceFinder.java
@@ -7,7 +7,6 @@ import java.net.URISyntaxException;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
